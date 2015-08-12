ed26e9c6e83414c2e45279aa65752520c57ac262
Valerij Wittenbeck <valerij.wittenbeck@student.kit.edu>
2015-08-12 13:53:22 +0200

    project searching now ignores the ArmarX prefix of some packages

--- project.sh
+++ project.sh
@@ -3,6 +3,7 @@
 IFS=";" read -a projects <<< "$AX_PROJECTS"
 
 AX_PROJECT_SEARCH_DIR="$ArmarX_DIR/$1"
+AX_PROJECT_SEARCH_DIR_PREFIX="$ArmarX_DIR/ArmarX$1"
 
 if [ -z $1 ]
 then
@@ -12,7 +13,7 @@ else
 	do
 		AX_PROJECT_DIR=$ArmarX_DIR/$i
 		AX_PROJECT_DIR_LC="${AX_PROJECT_DIR,,}"
-		if [ "${AX_PROJECT_DIR_LC:0:${#AX_PROJECT_SEARCH_DIR}}" = ${AX_PROJECT_SEARCH_DIR,,} ] 
+		if [ "${AX_PROJECT_DIR_LC:0:${#AX_PROJECT_SEARCH_DIR}}" = ${AX_PROJECT_SEARCH_DIR,,} ] || [ "${AX_PROJECT_DIR_LC:0:${#AX_PROJECT_SEARCH_DIR_PREFIX}}" = ${AX_PROJECT_SEARCH_DIR_PREFIX,,} ]
 		then
 			cd "$AX_PROJECT_DIR"
 			break
@@ -23,5 +24,6 @@ fi
 unset IFS
 unset projects
 unset AX_PROJECT_SEARCH_DIR
+unset AX_PROJECT_SEARCH_DIR_PREFIX
 unset AX_PROJECT_DIR
 unset AX_PROJECT_DIR_LC
